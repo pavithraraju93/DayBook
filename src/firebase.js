3078655b6be2689362c289e2ba66901d0c3ec4a3
pavithraraju93 <pavithraraju77@gmail.com>
2024-08-18 21:06:23 +0530

    commit

--- src/firebase.js
+++ src/firebase.js
@@ -3,15 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAHExHfMDRu2lVecgYxgW86TXDF-dvsWrY",
-//   authDomain: "personal-finance-tracker-ec56e.firebaseapp.com",
-//   projectId: "personal-finance-tracker-ec56e",
-//   storageBucket: "personal-finance-tracker-ec56e.appspot.com",
-//   messagingSenderId: "669303047696",
-//   appId: "1:669303047696:web:5b4b41cc896752c03e6c95",
-//   measurementId: "G-7X36E69HYW",
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyDlcJ_D91trfzHPatctAydgdDFlk3Y0sKQ",
